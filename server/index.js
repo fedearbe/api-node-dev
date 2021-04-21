5f663268bab704d06d8396a3735c252aa700a616
Fede <fedearbe@gmail.com>
2021-04-21 19:20:53 -0400

    Add cross-env and setting the enviroment to package.json

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello worlds!');
+  res.send("Hello from Fede's world!");
 });
 
 module.exports = app;
