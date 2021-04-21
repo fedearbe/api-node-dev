bca931a1950d01678384c65258195da52047df85
Fede <fedearbe@gmail.com>
2021-04-21 06:54:33 -0400

    Add ESLint and config file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { EWOULDBLOCK } = require('constants');
 const http = require('http');
 
-const hostname =  '127.0.0.1';
-const port = 3000; 
+const hostname = '127.0.0.1';
+const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
