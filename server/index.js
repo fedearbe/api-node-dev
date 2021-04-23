23f5d3828b519e346faed5e4b9b4a2949770dbbc
Fede <fedearbe@gmail.com>
2021-04-23 07:10:38 -0400

    Add middleware error

--- server/index.js
+++ server/index.js
@@ -6,4 +6,13 @@ app.get('/', (req, res) => {
   res.send("Hello from Fede's world!");
 });
 
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode);
+  res.json({
+    message,
+  });
+});
+
 module.exports = app;
