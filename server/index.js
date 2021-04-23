bf538d0d62527e35d2d3375c63d560c27fd6e6c0
Fede <fedearbe@gmail.com>
2021-04-23 08:07:26 -0400

    Add morgan to log all http request and combine de error message with winston

--- server/index.js
+++ server/index.js
@@ -1,17 +1,37 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const logger = require('./config/logger');
 
 const app = express();
 
+app.use(
+  morgan('combined', {
+    stream: { write: (message) => logger.info(message) },
+  })
+);
+
 app.get('/', (req, res) => {
   res.send("Hello from Fede's world!");
+});
+
+app.use((req, res, next) => {
+  const statusCode = 404;
+  const message = 'Route not found';
+
+  logger.warn(message);
+
+  res.status(statusCode);
+  res.json({
+    message,
+  });
 });
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   logger.error(message);
-  
+
   res.status(statusCode);
   res.json({
     message,
